b067e335fe8bf0c69953100edb96c7f9b1b979e0
Preksha Gandhi <prekshagandhi@gmail.com>
2021-09-29 14:43:07 -0400

    Added weather api

---
+++ assets/javascript/datapage.js
@@ -0,0 +1,26 @@
+function cityWeather(cityInput) {
+    var latLonURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=3e666a3d81484f1bb070cec8466f5dd9";
+    var lat;
+    var lon;
+    fetch(latLonURL)
+        .then(response =>response.json())
+        .then(function(data) {
+            console.log(data);
+            cityName = data.name;
+            lat = data.coord.lat;
+            lon = data.coord.lon;
+            weatherUrl(lat, lon, cityName);
+            console.log(lat);
+            console.log(lon);     
+    });
+    
+    function weatherUrl(lat, lon, cityName) {
+        var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&cnt=6&appid=3e666a3d81484f1bb070cec8466f5dd9";
+        fetch(weatherUrl)
+            .then(response =>response.json())
+            .then(function(data) {
+                console.log(data);
+                getWeather(data, cityName);
+        });
+    };
+};
