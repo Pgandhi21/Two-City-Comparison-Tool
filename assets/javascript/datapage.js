aafe481ef01daa10360cb86bf95308bbe120a337
Preksha Gandhi <prekshagandhi@gmail.com>
2021-09-30 09:41:20 -0400

    function that calls latitude and longitude of the city

--- assets/javascript/datapage.js
+++ assets/javascript/datapage.js
@@ -1,7 +1,10 @@
-function cityWeather(cityInput) {
+// Global variables
+var lat;
+var lon;
+
+// Gets latitude and longitude of the city
+function callLatLon(cityInput) {
     var latLonURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=3e666a3d81484f1bb070cec8466f5dd9";
-    var lat;
-    var lon;
     fetch(latLonURL)
         .then(response =>response.json())
         .then(function(data) {
@@ -9,23 +12,13 @@ function cityWeather(cityInput) {
             cityName = data.name;
             lat = data.coord.lat;
             lon = data.coord.lon;
-            var latLon {
+            var latLon = {
                 lat: lat,
                 lon: lon
             };
+            //Include all functions that depends on latitude and longitude of the city
             weatherUrl(lat, lon, cityName);
-            console.log(lat);
-            console.log(lon);    
-            return latLon; 
     });
-    
-    function weatherUrl(lat, lon, cityName) {
-        var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&cnt=6&appid=3e666a3d81484f1bb070cec8466f5dd9";
-        fetch(weatherUrl)
-            .then(response =>response.json())
-            .then(function(data) {
-                console.log(data);
-                getWeather(data, cityName);
-        });
-    };
-};
+}
+
+
